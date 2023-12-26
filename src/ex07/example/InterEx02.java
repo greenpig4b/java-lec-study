c5ff73a82e25483e2f8d48cdebce20f019840fd1
Kimjihun <you@example.com>
2023-12-26 17:47:26 +0900

    test001

--- src/ex07/example/InterEx02.java
+++ src/ex07/example/InterEx02.java
@@ -1,5 +1,5 @@
 package ex07.example;
-
+//변경
 interface EventListener{
     void action();
 }
@@ -11,9 +11,7 @@ class MyApp{
     }
 }
 
-
 // 개발자인 내가 구현
-
 
 public class InterEx02 {
     public static void main(String[] args) {
