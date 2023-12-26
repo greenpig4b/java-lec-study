07c1ce29eeeac005dd9bdcbe1065dc8169a2b509
Kimjihun <you@example.com>
2023-12-26 17:40:47 +0900

    상속연습

--- src/ex07/example/ExampleEx01.java
+++ src/ex07/example/ExampleEx01.java
@@ -2,57 +2,51 @@ package ex07.example;
 
 abstract class Mother{
     String name;
+    String id;
+
     public abstract void attack();
-    public abstract void attack(Mother a ,Mother b);
+
 }
 
 class brother extends Mother{
 
-    public brother(String name) {
+    public brother(String name , String id) {
         this.name = name;
+        this.id = id;
     }
 
     public void attack(){
         System.out.println(name + "번째 자식을 불러왔다");
     }
-
-    public void attack(Mother a ,Mother b){
-        System.out.println(a + "번쨰 brother가" + b + "번째 를 공격하였다");
-    };
 
 }
 
 class father extends Mother{
 
-    public father(String name) {
+    public father(String name,String id) {
         this.name = name;
+        this.id = id;
     }
 
     public void attack(){
         System.out.println(name + "번째 자식을 불러왔다");
     }
-
-    public void attack(Mother a ,Mother b){
-        System.out.println(name + "번째 father 가" + name+ "를 공격하였다");
-    };
 
 }
 
 public class ExampleEx01 {
 
 
-    class stop{
-        public stop(Mother a, Mother b){
-            System.out.println();
-        }
+    public static void gameStart(Mother a , Mother b){
+        System.out.println(a.name + "번쨰 " + a.id + "가 " + b.name + "번째 " + b.id + "를 가격하였다.");
     }
 
-
-
     public static void main(String[] args) {
-        Mother h1 = new brother("1");
-        Mother h2 = new father("2");
+        Mother h1 = new brother("1","brother");
+        Mother h2 = new father("2", "father");
+        Mother h3 = new father("5","father");
 
+        gameStart(h1,h3);
 
     }
 }
