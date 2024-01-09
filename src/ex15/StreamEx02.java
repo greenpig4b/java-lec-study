6bf126c5ddfb0b715d3008f9ffb8a51867be9c82
Kimjihun <you@example.com>
2024-01-09 09:03:00 +0900

    스트림 연습

---
+++ src/ex15/StreamEx02.java
@@ -0,0 +1,26 @@
+package ex15;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+// 크기가 정해져있을떄.
+public class StreamEx02 {
+    public static void main(String[] args) {
+
+        InputStreamReader ir = new InputStreamReader(System.in); // 키보드로 데이터받는 객체 생성
+        char[] ch = new char[4]; // 보조스트림
+
+        try {
+            ir.read(ch);
+
+            for (char c : ch){
+                System.out.println(c + " ");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
---
+++ src/ex15/StreamEx04.java
@@ -0,0 +1,20 @@
+package ex15;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+
+public class StreamEx04 {
+    public static void main(String[] args) {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        try {
+            bw.write("안녕\n"); // \n 줄바꿈을 위함이 아니라 없으면 통신이 되지않는다, 마지막이나 다 붙여줘야한다
+            bw.write("반갑습니다\n");
+            bw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
