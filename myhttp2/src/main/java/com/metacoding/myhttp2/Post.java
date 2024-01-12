e044488ae71374abd4463c5f2c2ab321f1dec0d5
Kimjihun <you@example.com>
2024-01-12 11:56:52 +0900

    Changes

---
+++ myhttp2/src/main/java/com/metacoding/myhttp2/Post.java
@@ -0,0 +1,19 @@
+package com.metacoding.myhttp2;
+
+import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+//@ jvm이보는 주석
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Post {
+    private int userId;
+    private int id;
+    private String title;
+    private boolean completed;
+}
